4b7653cf2d7da184792c50295f771eb9988a06d7
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-20 12:46:19 +0530

    sum of two numbers

---
+++ java/Practise Java/q1.java
@@ -0,0 +1,16 @@
+ class Addition 
+   {
+  public static void main(String[] args)
+  {
+  int a=10;
+  int b=5;
+   int c = a + b;
+  System.out.println("The sum of a and b is = "+c);
+  }
+  public static int add(int n1,int n2)
+  {
+  int x;
+  x = n1+n2;
+  return x;
+     }
+  }
