1a1188bd2608ee2108aa2476663f2a66d899dbde
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-20 23:14:17 +0530

    multiplication table without loops

---
+++ java/Practise Java/q.java
@@ -0,0 +1,20 @@
+ import java.util.*;
+public class multiple {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(n * 1);
+        System.out.println(n * 2);
+        System.out.println(n * 3);
+        System.out.println(n * 4);
+        System.out.println(n * 4);
+        System.out.println(n * 5);
+        System.out.println(n * 6);
+        System.out.println(n * 7);
+        System.out.println(n * 8);
+        System.out.println(n * 9);
+        System.out.println(n * 10);
+       
+    }
+}
+
