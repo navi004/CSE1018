2606ab368af5cb99f0b0cf4c7c111cb97907f88c
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-22 17:25:21 +0530

    Primitive data types

---
+++ java/Practise Java/Datatypes.java
@@ -0,0 +1,34 @@
+
+   //PRIMITIVE DATA TYPES
+public class Lfd {
+    public static void main(String[] args) {
+        byte myNum = 100; //Byte[-128 to 127]
+        
+        short s = 5000; //Short size [-32768 to 32767]
+        
+       float f = 21.55f;  // Floating point number
+       
+        double d = 21.556677d; 
+        // the value should end with an "f" for floats and "d" for doubles
+       
+        long l = 1000000L;
+        //should end the value with an "L"
+        
+        int a = 12; //integer(Whole number)
+        
+        char character = 'n' ; //character
+        
+        String name = "Naveen" ; //String
+       
+        System.out.println(f);
+       System.out.println(d);
+       System.out.println(l);
+         System.out.println(s);
+          System.out.println(myNum);
+         System.out.println(name);
+         System.out.println(a);
+         System.out.println(character);
+        
+       
+       }
+      }
