e3d95ee44a7c54f1516c03d50b0cc715b47fc827
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-23 12:42:01 +0530

    to find area of rectangle with user defined method

--- java/Practise Java/Q7.java
+++ java/Practise Java/Q7.java
@@ -16,3 +16,29 @@ public class Shape {
         System.out.println("Area of your rectangle is " + myArea);
     }
  }
+
+
+
+//withn input
+import java.lang.*;
+import java.util.Scanner;
+class Rectangle{
+    float length,breadth;
+    public float findArea(float a, float b) {
+        length = a;
+        breadth = b;
+        float area = length * breadth;
+        return area;
+    }
+}
+
+public class Shape {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        Rectangle rect = new Rectangle();
+        float myArea = rect.findArea(a,b);
+        System.out.println("Area of your rectangle is " + myArea);
+    }
+ }
