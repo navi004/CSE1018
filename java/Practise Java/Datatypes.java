0d923a77780987993822b9daf76d681b14a3392c
navi004 <115283282+navi004@users.noreply.github.com>
2023-02-26 22:49:48 +0530

    Primitive datatypes

--- java/Practise Java/Datatypes.java
+++ java/Practise Java/Datatypes.java
@@ -1,4 +1,26 @@
-
+//Java program to demonstrate the basic datatypes in Java
+/* Primitive Data types in Java
+ * Integer - byte, short, int, long
+ * Floating Point - float, double
+ * Character - char
+ * Boolean - boolean
+ * 
+ * Java supports both positive and negative numbers.
+ * There is no concept of "unsigned" integers in Java
+ * 
+ * The memory allocation for each datatype is 
+ * byte - 1 byte [1 byte = 8 bits. 2^8=256. So a byte datatype can store 256 values. They are -128 to 127]
+ * short - 2 bytes
+ * int - 4 bytes
+ * long - 8 bytes [You should add 'L' or 'l' at the end while defining a new 'long' value. e.g., long ab = 121218734L]
+ * 
+ * float - 4 bytes [You should add 'F' or 'f' at the end while defining a new 'float' value. e.g., float ab = 12.34F]
+ * double - 8 bytes
+ * 
+ * char - 1 byte
+ * 
+ * boolean - 1 bit  (true or false)
+ */
    //PRIMITIVE DATA TYPES
 public class Lfd {
     public static void main(String[] args) {
