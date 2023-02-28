2d0bfdfc0c82698073c0f178981b9f0a576efa59
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-01 00:05:55 +0530

    to print Inverted 0-1 triangle

---
+++ java/Practise Java/Patterns.java/Inverted 0-1 triangle.java
@@ -0,0 +1,39 @@
+//java program to print 0-1 triangle
+/*
+10101
+0101
+101
+01
+1
+ */
+import java.util.*;
+
+public class Patterns {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the n value:");
+        int n = input.nextInt();
+         
+        //outer loop is for number of rows
+        for(int i=n; i>=1; i--) {
+             
+            //inner loop is for number of columns
+            //cell--> (i,j) using the matrix method
+            for(int j=1; j<=i; j++) {
+                int sum = i+j;         // assinging the sum to sum of i and j
+               if(sum % 2 == 0) {
+                System.out.print("1");    //print 1 if sum is even
+               }
+               else {
+                System.out.print("0");    //print 0 if sum is odd
+               }
+            }
+            //to end the line after each row
+            System.out.println("");
+        }
+    }
+}
+
+
+
+
