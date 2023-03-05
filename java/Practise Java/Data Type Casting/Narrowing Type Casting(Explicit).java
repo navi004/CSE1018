e1c5298cc4132d453f66eaae555917da642e2cb2
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-05 22:55:12 +0530

    Explicit type casting

--- java/Practise Java/Data Type Casting/Narrowing Type Casting(Explicit).java
+++ java/Practise Java/Data Type Casting/Narrowing Type Casting(Explicit).java
@@ -20,3 +20,30 @@
     System.out.println(" The integer value: " + data);
   }
 }
+
+//conversion from int to string
+class Main {
+  public static void main(String[] args) {
+    // create int type variable
+    int num = 10;
+    System.out.println("The integer value is: " + num);
+
+    // converts int to string type
+    String data = String.valueOf(num);
+    System.out.println("The string value is: " + data);
+  }
+}
+
+//conversion from string to intclass Main {
+  public static void main(String[] args) {
+    // create string type variable
+    String data = "10";
+    System.out.println("The string value is: " + data);
+
+    // convert string variable to int
+    int num = Integer.parseInt(data);
+    System.out.println("The integer value is: " + num);
+  }
+}
+
+
