7c84f1ced34e27a1b8f659868396aae4dca9f8b5
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-08 00:24:26 +0530

    to print stars in rotated half pyramid pattern

---
+++ java/Practise Java/HomeWorkSums/Q4(HW-3).java
@@ -0,0 +1,39 @@
+// Java code to demonstrate star pattern
+/*
+           * 
+         * * 
+       * * * 
+     * * * * 
+   * * * * * 
+ */
+
+import java.util.*;
+
+public class Firstclass {
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter the n value:");
+      int n = input.nextInt();
+      
+     // outer loop to handle number of rows
+        //  n in this case
+      for(int i=1;i<=n;i++) {
+        
+          // inner loop to handle number spaces
+            // values changing acc. to requirement
+         for(int j=1;j<=n-i;j++) {
+            // printing spaces
+            System.out.print("  ");
+         }
+        
+         //  inner loop to handle number of columns
+            //  values changing acc. to outer loop
+         for(int j=1;j<=i;j++) {
+            // printing stars
+            System.out.print("* ");
+         }
+        // ending line after each row
+         System.out.println();
+      }
+   }
+}
