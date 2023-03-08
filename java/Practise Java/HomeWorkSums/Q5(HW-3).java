61b6f4c45958d6f14e10d15f5fa11de6c1abf0cd
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-08 14:47:31 +0530

    to print pascal's trinagle

--- java/Practise Java/HomeWorkSums/Q5(HW-3).java
+++ java/Practise Java/HomeWorkSums/Q5(HW-3).java
@@ -6,25 +6,32 @@
   1 3 3 1
  1 4 6 4 1
  */
-
 import java.util.*;
+
 public class Patterns {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the n value:");
         int n = input.nextInt();
 
-        for(int i=0; i<n; i++) {
-
-            for(int j=0; j<n-i; j++) {
+        // loop through each row of Pascal's triangle
+        for (int i = 0; i < n; i++) {
+            // print spaces before each row
+            for (int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
+
             int number = 1;
-            for(int j=0; j<=i; j++) {
+            // calculate and print each element of the current row
+            for (int j = 0; j <= i; j++) {
                 System.out.print(number + " ");
-                number = number*(i-j)/(j+1) ;
+                // calculate the next number using the formula
+                number = number * (i - j) / (j + 1);
             }
+
+            // move to the next line
             System.out.println();
         }
     }
 }
+
