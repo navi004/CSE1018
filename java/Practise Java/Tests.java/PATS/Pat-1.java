85e4000cb51f710121f68ffda76e4c7161a34813
navi004 <115283282+navi004@users.noreply.github.com>
2023-03-13 23:43:24 +0530

    Update Pat-1.java

--- java/Practise Java/Tests.java/PATS/Pat-1.java
+++ java/Practise Java/Tests.java/PATS/Pat-1.java
@@ -5,7 +5,7 @@ public class Main {
     int piercingDamage,slashDamage,playerXP;
     String swordName;
   //creating constructors with parameter
-     Main(String swordName) {
+      Main(String swordName) {
        //refer the instance variable to intialized variable
         this.swordName = swordName;           
     }
@@ -14,12 +14,16 @@ public class Main {
         System.out.println("Start The Game");
     }
     //method to attack
-    public void attack() {
+    public void attack(Main opponent) {
         System.out.println("Player Attacked Using:"+swordName);
+                  // decreasing the player 2 XP for player1 attack
+          opponent.playerXP = opponent.playerXP-piercingDamage;
     }
     //method to parry
-    public void parry() {
+    public void parry(Main opponent) {
         System.out.println("Player Parried Using:"+swordName);
+                  // decreaing the player1 XP due to player2 parry
+        opponent.playerXP = opponent.playerXP-slashDamage;
     }
     //method to get the features of the players
     public void getWeaponFeatures(int a,int b,int c) {
@@ -38,31 +42,33 @@ public class Main {
   // main method
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
-          //create two objects for two players
-            Main player1 = new Main("Valkairy");     //assinging inputs to constructors
-            Main player2 = new Main("Viking");
+            String sName = input.nextLine();
+            //create two objects for two players
+            Main player1 = new Main(sName);   //assinging inputs to constructors
+            sName = input.nextLine();
+            Main player2 = new Main(sName);
           //calling the start the game method
             startTheGame();
           // taking input values of player 1 features
             player1.piercingDamage = input.nextInt();
             player1.slashDamage = input.nextInt();
-            player1.slashDamage = input.nextInt();
+            player1.playerXP = input.nextInt();
           // calling the get weapon features method with input values of player 1
-            player1.getWeaponFeatures(player1.piercingDamage,player1.slashDamage,player1.slashDamage);
+            player1.getWeaponFeatures(player1.piercingDamage,player1.slashDamage,player1.playerXP);
            // taking input values of player 2 features
             player2.piercingDamage = input.nextInt();
             player2.slashDamage = input.nextInt();
-            player2.slashDamage = input.nextInt();
+            player2.playerXP = input.nextInt();
           // calling the get weapon features method with input values of player 2
-            player2.getWeaponFeatures(player2.piercingDamage,player2.slashDamage,player2.slashDamage);
+            player2.getWeaponFeatures(player2.piercingDamage,player2.slashDamage,player2.playerXP);
           // calling the attack method 
-            player1.attack();
-          // decreasing the player 2 XP for player1 attack
-            player2.playerXP = player2.playerXP-player1.piercingDamage;
+            player1.attack(player2);
+                  // decreasing the player 2 XP for player1 attack
+                  //player2.playerXP = player2.playerXP-player1.piercingDamage;
           // calling the parry method
-            player2.parry();
-          // decreaing the player1 XP due to player2 parry
-            player1.playerXP = player1.playerXP-player2.slashDamage;
+            player2.parry(player1);
+                  // decreaing the player1 XP due to player2 parry
+                  //player1.playerXP = player1.playerXP-player2.slashDamage;
           // calling the end function
             endTheGame();
             
@@ -75,8 +81,11 @@ public class Main {
             if(player1.playerXP>player2.playerXP) {
                 System.out.println("Player 1 is Winner");
             }
-            else{
+            else if(player1.playerXP<player2.playerXP){
                 System.out.println("Player2 is Winner");
+            }
+            else {
+              System.out.println("TIE");
             }
         }
 
