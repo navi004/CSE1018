95025735bebced206be7f333d6a41a69a4415d9f
navi004 <115283282+navi004@users.noreply.github.com>
2023-04-12 23:49:30 +0530

    Create StringvalueOf.java

---
+++ java/Practise Java/Data Type Casting/StringvalueOf.java
@@ -0,0 +1,16 @@
+// to convert any datatype to string using String.valueOf
+class Main {
+  public static void main(String[] args) {
+    int a = 5;
+    long l = -2343834L;
+    float f = 23.4f;
+    double d = 923.234d;
+
+    // convert numbers to strings
+    System.out.println(String.valueOf(a));  // "5"
+    System.out.println(String.valueOf(l));  // "-2343834"
+    System.out.println(String.valueOf(f));  // "23.4"
+
+    System.out.println(String.valueOf(d));  // "923.234"
+  }
+}
