a2cb801373e6fc15c46f25674a8d5a87e763fad4
navi004 <115283282+navi004@users.noreply.github.com>
2023-05-23 15:20:55 +0530

    Create FileWrite.java

---
+++ java/Practise Java/FileHandling java/FileWrite.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+class FileWrite {
+	public static void main(String[] args) {
+		File file = new File("/home/student/22mia1049/Java1049/naveen1.txt");
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+		FileWriter writer = new FileWriter(file);
+		//ASCII
+		Writer.write(98);
+		
+		Writer.write("Naveen");
+		writer.flush();
+		writer.close();
+		
+	}
+}
