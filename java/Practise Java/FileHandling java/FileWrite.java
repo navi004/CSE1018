6a6c3d37a1d24a5ce93f42c9c5971fb31989f787
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-05-25 12:31:37 +0530

    Update FileWrite.java

--- java/Practise Java/FileHandling java/FileWrite.java
+++ java/Practise Java/FileHandling java/FileWrite.java
@@ -3,44 +3,24 @@ import java.io.*;
 class FileWrite {
 	public static void main(String[] args) {
 		File file = new File("/home/student/22mia1049/Java1049/naveen1.txt");
-		if(!file.exists()) {
-			try {
-				file.createNewFile();
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
-		FileWriter writer = new FileWriter(file);
-		//ASCII
-		Writer.write(98);
-		
-		Writer.write("Naveen");
-		writer.flush();
-		writer.close();
-		
-	}
-}
-
-
-import java.io.*;
-
-class FileWrite {
-	public static void main(String[] args) {
 	try {
-		File file = new File("/home/student/22mia1049/Java1049/naveen1.txt");
-		
 		file.createNewFile();
 		FileWriter writer = new FileWriter(file);
 		//ASCII
-		Writer.write(98);
+		writer.write(98);  //prints ASCII value 98 = b
 		
-		Writer.write("Naveen");
+        //write() method is used to write contents in the file
+		writer.write("Naveen");
+
 		writer.flush();
+        //flush method will pass all the contents to the file
+
 		writer.close();
+        //close your file
 	}
+
 	catch(IOException e) {
-	e.printStackTrace();
+	    e.printStackTrace();
 	}
 	
 		
