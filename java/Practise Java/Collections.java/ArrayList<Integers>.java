79f87ee6318412f93bad6b6c3c8c8de83c5c274a
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-06-01 12:37:23 +0530

    Create ArrayList<Integers>.java

---
+++ java/Practise Java/Collections.java/ArrayList<Integers>.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(10);
+        nums.add(9);
+        nums.add(3);
+        nums.add(1);
+    
+        Iterator<Integer> itr = nums.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+}
