42f29db21566f7827070e89ee76238a96ac45d5d
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-06-01 12:38:31 +0530

    Create Integer ArrayList_Without Iterator.java

---
+++ java/Practise Java/Collections.java/Integer ArrayList_Without Iterator.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(10);
+        nums.add(9);
+        nums.add(3);
+        nums.add(1);
+    
+        // Iterator<Integer> itr = nums.iterator();
+        // while(itr.hasNext()) {
+        //     System.out.println(itr.next());
+        // }
+        System.out.println(nums);
+    }
+}
