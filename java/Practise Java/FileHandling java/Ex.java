5e2df525861f6bdb287847d20d49196acb1ee155
NaveenNidadavolu <115283282+navi004@users.noreply.github.com>
2023-06-04 21:37:12 +0530

    Update Ex.java

--- java/Practise Java/FileHandling java/Ex.java
+++ java/Practise Java/FileHandling java/Ex.java
@@ -1,9 +1,30 @@
 import java.io.*;
 
-class Demoo {
+
+class Demo {
     public static void main(String[] args) {
         File file1 = new File("C:\\Users\\nvkaj\\OneDrive\\Pictures\\Temp\\temp1");
+        System.out.println(file1.exists());
+        if (!file1.exists()) {
+            file1.mkdir();
+        }
+        System.out.println(file1.exists());
         try {
+            File file = new File("C:\\Users\\nvkaj\\OneDrive\\Pictures\\Temp\\temp1\\program.txt");
+            if (!file.exists()) {
+                file.createNewFile();
+                System.out.println("File Created Successfully");
+            }
+            File newname = new File("C:\\Users\\nvkaj\\OneDrive\\Pictures\\Temp\\temp1\\naveen.txt");
+            if (file.renameTo(newname)) {
+                System.out.println("File Renamed Successfully");
+            }
+            System.out.println(file.getName());
+
+            System.out.println(file.canRead());
+            System.out.println(file.canWrite());
+            System.out.println(file.canExecute());
+
             String[] mylist = file1.list();
             for (int i = 0; i < mylist.length; i++) {
                 System.out.println(mylist[i]);
@@ -16,7 +37,7 @@ class Demoo {
                     System.out.println(f);
                 }
             }
-            File file = new File("C:\\Users\\nvkaj\\OneDrive\\Pictures\\Temp\\temp1\\program.txt");
+
             FileWriter fw = new FileWriter(file, true);
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write("Naveen.");
@@ -54,4 +75,17 @@ class Demoo {
             BufferedReader br2 = new BufferedReader(fr2);
 
             int output = br2.read();
+            char[] ch = new char[(int) file.length()];
+            System.out.println("Number of characters with char: ");
+            System.out.println(ch.length);
+
+            file.delete();
+            if (!file.exists()) {
+                System.out.println("File deleted");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
  
